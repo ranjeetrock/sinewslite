72927a453650cff8727484611f84a4788c801050
Your Name <you@example.com>
2025-08-13 16:22:22 +0530

    Update dependencies for Render deployment

--- news/views.py
+++ news/views.py
@@ -224,7 +224,7 @@ from .api_utils import get_breaking_news
 
 def home(request):
     breaking_news = get_breaking_news()
-    print("Fetched News:", breaking_news)  # This will show in terminal
+    # print("Fetched News:", breaking_news)  # This will show in terminal
     return render(request, 'news/home.html', {'breaking_news': breaking_news})
 
 
@@ -232,8 +232,8 @@ def home(request):
 
 
 
-import requests
-from django.conf import settings
+# breaking news
+
 
 def get_breaking_news():
     url = 'https://newsapi.org/v2/top-headlines'
@@ -255,7 +255,7 @@ def get_breaking_news():
 
 
 
-
+# time  conveter
 
 
 def convert_utc_to_ist(utc_time_str):
