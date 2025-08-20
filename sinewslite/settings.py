eb3f47e6ab9e1c1c2ee5e1a196ea776187a7ca72
Your Name <you@example.com>
2025-08-20 18:33:10 +0530

    Update dependencies for Render deployment

--- sinewslite/settings.py
+++ sinewslite/settings.py
@@ -140,7 +140,7 @@ STATICFILES_DIRS = [BASE_DIR / "static"]  # ✅ points to your custom static dir
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
-# MEDIA_URL = '/media/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
