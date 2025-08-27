d899bdbda74ff571d398c8f04a371ca1395001d6
Your Name <you@example.com>
2025-08-27 18:44:40 +0530

    Update dependencies for Render deployment

--- sinewslite/urls.py
+++ sinewslite/urls.py
@@ -4,11 +4,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from news.admin import create_temp_admin   
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-     path("create-temp-admin/", create_temp_admin),  # 🚨 temporary URL
+    
    path('', include('news.urls', namespace='news')),
    
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
